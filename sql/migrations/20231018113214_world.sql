b4ae3cf3953ca2059b50a3c1ed489f24ebe14ff5
itswicky <blaindes51@gmail.com>
2023-10-19 06:37:21 -0500

    Core/SQL: Migrations

---
+++ sql/migrations/20231018113214_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE \`add_migration\`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM \`migrations\` WHERE \`id\`='$DATE');
+IF v=0 THEN
+INSERT INTO \`migrations\` VALUES ('$DATE');
+-- Add your query below.
+
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
